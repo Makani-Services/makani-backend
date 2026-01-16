ff84b771dacddaf8fa4b1581bd1c8f00ea126b99
Han Ming <han.miing555@gmail.com>
2026-01-17 02:21:06 +0900

    chore: version code to 2.0.1

--- src/scheduled-task/scheduled-task.controller.ts
+++ src/scheduled-task/scheduled-task.controller.ts
@@ -1,27 +1,45 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Query } from '@nestjs/common';
 import { ScheduledTaskService } from './scheduled-task.service';
+import { UserService } from 'src/user/user.service';
 
 @Controller('api/scheduled-task')
 export class ScheduledTaskController {
-  constructor(private readonly scheduledTaskService: ScheduledTaskService) {}
+  constructor(
+    private readonly scheduledTaskService: ScheduledTaskService,
+    private readonly userService: UserService,
+  ) {}
 
   //iOS
   @Get('get_version_info/ios')
-  async getiOSVersionInfo() {
+  async getiOSVersionInfo(@Query() query: any) {
+    const { userId } = query;
+    const user = await this.userService.getUserById(userId);
+
+    let force_update = false;
+    if (user.branches[0].name === 'Hawaii') {
+      force_update = false;
+    }
     return {
       platform: 'ios',
-      latest_version: '2.0.0',
-      force_update: true,
+      latest_version: '2.0.1',
+      force_update: force_update,
     };
   }
 
   //Android
   @Get('get_version_info/android')
-  async getAndroidVersionInfo() {
+  async getAndroidVersionInfo(@Query() query: any) {
+    const { userId } = query;
+    const user = await this.userService.getUserById(userId);
+
+    let force_update = false;
+    if (user.branches[0].name == 'Hawaii') {
+      force_update = false;
+    }
     return {
       platform: 'android',
-      latest_version: '2.0.0',
-      force_update: true,
+      latest_version: '2.0.1',
+      force_update: force_update,
     };
   }
 
