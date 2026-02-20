3cf1b45bdd45bbedf0efaf1d9cccadd74023e50a
Han Ming <han.miing555@gmail.com>
2026-02-20 17:26:44 +0900

    minor update

--- src/wo/wo.service.ts
+++ src/wo/wo.service.ts
@@ -2002,7 +2002,7 @@ export class WoService extends TypeOrmCrudService<WoEntity> {
     orders['customerNotes'] = await this.customerNoteService.getAllByWoId(id);
     orders['attachments'] = await this.woAttachmentRepo.find({
       where: { wo: { id: id } },
-      relations: ['tags'],
+      relations: ['tags', 'uploadedBy', 'uploadedByCustomerUser'],
     });
 
     return orders;
